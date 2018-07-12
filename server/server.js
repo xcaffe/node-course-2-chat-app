d84a0568585a95b7fa869c2fc9821bd711cc2b2f
faithful <ahamefulefaithful@gmail.com>
2018-07-12 10:20:41 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat app',
+    createdAt: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+});
 
     socket.on ('createMessage', (message)  =>{
         console.log('createMessage', message);
+         
         io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-    });
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+      });
+
+      // socket.broadcast.emit('newMessage', {
+      // from: message.from,
+     // text: message.text,
+     // createdAt: new Date().getTime()
+     // });
 });
 
     socket.on('disconnect', () => {
@@ -29,7 +47,7 @@ io.on('connection', (socket) => {
         
         });
     });
-    
+
 server.listen(port, () => {
     console.log(`Server is up on ${port} `);
 });
